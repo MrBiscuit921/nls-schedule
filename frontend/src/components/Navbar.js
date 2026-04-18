4c89aaec6d3744dc738c4622325b9a77c6b0966e
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2026-04-18 14:42:24 +1200

    linked GPS Tracker

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,6 +10,9 @@ export default function Navbar() {
     return (
         <nav style={{ padding: "1rem", background: "#1e1e2f", color: "white", display: "flex", gap: "1rem" }}>
             <Link to="/" style={{ color: "white", textDecoration: "none" }}>Schedule</Link>
+            <Link to="https://nords-gps.vercel.app/" target="_blank" rel="noopener noreferrer" style={{ color: "white", textDecoration: "none" }}>
+                GPS Tracker
+            </Link>
             {/* {token ? (
                 <>
                     <Link to="/admin" style={{ color: "white", textDecoration: "none" }}>Admin Panel</Link>
